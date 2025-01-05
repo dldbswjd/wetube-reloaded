dd9838ce824ee83b3a345b6ad9e00579005cc11e
yunjeongleekr <yunjeongleekr@gmail.com>
2025-01-05 13:09:25 +0900

    #5.2

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
+
+const PORT = 4000;
+
+console.log(process.cwd());
+
+const app = express();
+const logger = morgan("dev");
+
+app.set("view engine", "Pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+const handleListening = () =>
+  console.log(`Server listenting on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
