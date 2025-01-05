23ba974fe7750280458468c9b2a21a5f840c653d
yunjeongleekr <yunjeongleekr@gmail.com>
2025-01-05 17:14:23 +0900

    #5.7 condotianal

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,12 @@
-export const trending = (req, res) => res.render("home");
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => {
-  console.log(req.params);
-  res.send("Watch");
+const fakeUser = {
+  username: "Nico",
+  loggedIn: false,
 };
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", fakeUser });
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search User");
 export const upload = (req, res) => res.send("Upload Video");
 export const deleteVideo = (req, res) => {
