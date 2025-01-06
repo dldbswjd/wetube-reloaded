27d69aacea7baedb61988c1f8306acbceff86b5f
yunjeongleekr <yunjeongleekr@gmail.com>
2025-01-06 23:10:51 +0900

    #6.0

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,39 @@
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    commets: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    commets: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    commets: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  res.render("home", { pageTitle: "Home", videos });
+  return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const see = (req, res) => {
+  //   const id = req.params.id;
+  const { id } = req.params; //ES6
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}` });
+};
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search User");
 export const upload = (req, res) => res.send("Upload Video");
