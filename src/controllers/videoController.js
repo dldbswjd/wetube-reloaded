4eeed7bc9bb1d470762176901dfae5796f2c3f1a
yunjeongleekr <yunjeongleekr@gmail.com>
2025-01-19 15:59:37 +0900

    #6.27

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -75,8 +75,15 @@ export const deleteVideo = async (req, res) => {
   return res.redirect("/");
 };
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
   const { keyword } = req.query;
-  console.log("should search for ", keyword);
-  return res.render("search", { pageTitle: "Search" });
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(`${keyword}$`, "ig"),
+      },
+    });
+  }
+  return res.render("search", { pageTitle: "Search", videos });
 };
